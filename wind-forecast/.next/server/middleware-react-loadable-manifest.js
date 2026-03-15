53d6e52789bddbe09235032e63ad5483df478c73
Praneeth gupta <nitinkumariit4@gmail.com>
2026-03-15 14:28:45 +0530

    Delete wind-forecast/.next directory

--- wind-forecast/.next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app\\\\page.tsx -> @/components/WindChart\":{\"id\":\"app\\\\page.tsx -> @/components/WindChart\",\"files\":[\"static/chunks/_app-pages-browser_components_WindChart_tsx.js\"]}}"
--- wind-forecast/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WUkVBO7wJ1qTTBq/ekH+fBUV6Lmx3tuOce1Kk5PWuck=\"\n}"
